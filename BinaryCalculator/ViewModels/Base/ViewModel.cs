f23a87c6426ac00270417d555054ffea51e29123
Exvise <e.danilkov@gmail.com>
2022-04-29 13:08:12 +0300

    Added view model, changed proj name

---
+++ BinaryCalculator/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BinaryCalculator.Application.ViewModels.Base
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string propName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChanged(propName);
+
+            return true;
+        }
+    }
+}
